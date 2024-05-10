0b7840a5523815e02cbfeee209f360865c3f0d31
Yair Zadok <yair.zad@gmail.com>
2024-05-10 01:02:40 -0400

    Update expense_poster.go

--- receipt_backend/expense_poster.go
+++ receipt_backend/expense_poster.go
@@ -22,13 +22,13 @@ func get_categories() error {
 		log.Println("No realm ID.  QBO calls only work if the accounting scope was passed!")
 		return errors.New("Realm ID empty")
 	}
-    sql_query := "SELECT Name FROM Account WHERE Classification='Expense'"
-    sql_query = url.QueryEscape(sql_query)
+	sql_query := "SELECT Name FROM Account WHERE Classification='Expense'"
+	sql_query = url.QueryEscape(sql_query)
 
-    request, err := http.NewRequest("GET", config.OAuthConfig.IntuitAccountingAPIHost+"/v3/company/"+realmId+"/query?query="+sql_query, nil)
+	request, err := http.NewRequest("GET", config.OAuthConfig.IntuitAccountingAPIHost+"/v3/company/"+realmId+"/query?query="+sql_query, nil)
 	if err != nil {fmt.Println(err)}
     
-    request.Header.Set("Content-Type", "text/plain")
+	request.Header.Set("Content-Type", "text/plain")
 	request.Header.Set("accept", "application/json")
 	accessToken := cache.GetFromCache("access_token")
 	request.Header.Set("Authorization", "Bearer "+accessToken)
@@ -37,11 +37,11 @@ func get_categories() error {
 
 	if err != nil {fmt.Println(err)}
 	
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {fmt.Println(err)}
 
-    responseString := string(body)
+	responseString := string(body)
 	log.Println(responseString)
 
 	return nil
